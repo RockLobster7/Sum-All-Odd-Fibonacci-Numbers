3b237e3acb2b6ec7bca7423c0ab78e5cb23894be
Mario Cueva <mario.cueva.au@gmail.com>
2017-09-25 21:08:37 +1000

    completed challenge

--- Sum-All-Odd-Fibonacci-Numbers.js
+++ Sum-All-Odd-Fibonacci-Numbers.js
@@ -8,17 +8,42 @@ For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less
 
 */
 
+// function fibonacci(num) {
+//     if (num <= 1) return 1;
+//     return fibonacci(num - 1) + fibonacci(num - 2);
+// }
+
+//0 1 1 2 3 5 8 13 21
+
+'strict';
 
 function sumFibs(num) {
-    return num;
-  }
+    var previousNumber = 0;
+    var currentNumber = 1;
+    var result = 0;
 
-  
-  console.log(sumFibs(1)); // should return a number.
-  console.log(sumFibs(1000)); // should return 1785.
-  console.log(sumFibs(4000000)); // should return 4613732.
-  console.log(sumFibs(4)); // should return 5.
-  console.log(sumFibs(75024)); // should return 60696.
-  console.log(sumFibs(75025)); // should return 135721.
-      
-  
+    do {
+
+        //test if the current number is odd - if so add it to the result
+        if (currentNumber % 2 !== 0) {
+            result += currentNumber;
+        }
+
+        //increment fibonacci sequence
+        currentNumber =  previousNumber + currentNumber;
+        previousNumber = currentNumber - previousNumber;
+
+        // test if we reached the input number. If no go back and increment fibonacci
+    } while (currentNumber <= num)
+
+    return result;
+}
+
+
+
+console.log(sumFibs(1)); // should return a number.
+console.log(sumFibs(1000)); // should return 1785.
+console.log(sumFibs(4000000)); // should return 4613732.
+console.log(sumFibs(4)); // should return 5.
+console.log(sumFibs(75024)); // should return 60696.
+console.log(sumFibs(75025)); // should return 135721.
